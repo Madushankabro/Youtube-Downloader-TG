9d3376b02e99e7b322d8fe521a2a90e332cbf77d
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-17 08:32:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,5 +12,5 @@ async def start(client, message):
             "📣 Support channel 📣",url="https://t.me/danumabots")]
     ])
     thumbnail_url = "https://telegra.ph/file/0777bb5a6b5e947a1d9af.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"Hey<b>{message.from_user.first_name}.I'm UTUBE DOWNLODER BOT.</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
+    await message.reply_photo(thumbnail_url, caption=f"Hey<b>{message.from_user.first_name}.I'm UTUBE DOWNLODER BOT.</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **E PUSTHAKALAYA BOTs™** ❞ ─────\n ", reply_markup=Lasiya)
     raise StopPropagation
