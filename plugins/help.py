8d4d625f16055a84ffae60f67a21a5d4991af0ff
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-17 10:23:33 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,4 +7,4 @@ async def start(client, message):
 		[InlineKeyboardButton("Youtube tutorial", url="https://youtu.be/l6rTppg3a1w")],
 		 ])
 	help_image = "https://telegra.ph/file/2a5b44ea2f0a4b0a03f41.jpg"
-	await message.reply_photo(help_image,  caption="**♔English help**\n• Just Send your Youtube video url. \n• And I will give Method list to select your choice.\n\n**♔සිංහල උදව්**\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n\n • Powered By;\n• E PUSTHAKALAYA BOTs™.",reply_markup=Lasiya2)
+	await message.reply_photo(help_image,  caption="**♔English help**\n• Just Send your Youtube video url. \n• And I will give Method list to select your choice.\n\n**♔සිංහල උදව්**\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n\n • Powered By;\n• @epusthakalaya_bots.",reply_markup=Lasiya2)
