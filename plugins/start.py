d250c5baf990bb11238a69c366a697b6de361a70
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-17 08:30:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,12 +6,11 @@ async def start(client, message):
     # return
     Lasiya = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("Youtube ❤", url="https://www.youtube.com/channel/UCHyseVcfusXkOClpwja00yg")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/Danuma_admin_bot")],
         [InlineKeyboardButton(
-            "Bot channel 🧪",url="https://t.me/danumabots")]
+            "📣 Support channel 📣",url="https://t.me/danumabots")]
     ])
-    thumbnail_url = "https://telegra.ph/file/69a96df53932f1cd2174f.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"Hi<b>{message.from_user.first_name}</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
+    thumbnail_url = "https://telegra.ph/file/0777bb5a6b5e947a1d9af.jpg"
+    await message.reply_photo(thumbnail_url, caption=f"Hey<b>{message.from_user.first_name}.I'm UTUBE DOWNLODER BOT.</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
     raise StopPropagation
