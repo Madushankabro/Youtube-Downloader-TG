31d5d10cafaf1bad1b7715991f48bc4ed03004d5
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-17 10:11:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,9 +7,9 @@ async def start(client, message):
     Lasiya = InlineKeyboardMarkup([
         
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/Danuma_admin_bot")],
+            "👤 Developer 👤", url="https://t.me/kasu_bro_bot")],
         [InlineKeyboardButton(
-            "📣 Support channel 📣",url="https://t.me/danumabots")]
+            "📣 Support channel 📣",url="https://t.me/epusthakalaya_bots")]
     ])
     thumbnail_url = "https://telegra.ph/file/0777bb5a6b5e947a1d9af.jpg"
     await message.reply_photo(thumbnail_url, caption=f"Hey<b>{message.from_user.first_name}.I'm UTUBE DOWNLODER BOT.</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **E PUSTHAKALAYA BOTs™** ❞ ─────\n ", reply_markup=Lasiya)
